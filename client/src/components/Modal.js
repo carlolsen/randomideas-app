dd65658fb2779467abdadde320388abe0b45a2a2
Carl Olsen <carl-olsen@mchsi.com>
2024-03-12 16:07:12 -0500

    started client app

---
+++ client/src/components/Modal.js
@@ -0,0 +1,24 @@
+class Modal {
+  #modal = document.querySelector('#modal');
+  #modalBtn = document.querySelector('#modal-btn');
+  constructor() {
+    this.addEventListeners();
+  }
+  addEventListeners() {
+    this.#modalBtn.addEventListener('click', this.open.bind(this));
+    window.addEventListener('click', this.outsideClick.bind(this));
+    document.addEventListener('closemodal', () => this.close());
+  }
+  open() {
+    this.#modal.style.display = 'block';
+  }
+  close() {
+    this.#modal.style.display = 'none';
+  }
+  outsideClick(e) {
+    if (e.target === this.#modal) {
+      this.close();
+    }
+  }
+}
+export default Modal;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5002;
 const connectDB = require('./config/db');
 connectDB();
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
